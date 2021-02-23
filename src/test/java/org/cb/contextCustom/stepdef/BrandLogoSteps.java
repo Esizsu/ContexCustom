bb25ca2dae38e2536b75bfd33f795378e28b6f60
Esizsu <esra_izgi@hotmail.com>
2021-02-22 22:44:32 -0800

    added methods on BrandLogoSteps

--- src/test/java/org/cb/contextCustom/stepdef/BrandLogoSteps.java
+++ src/test/java/org/cb/contextCustom/stepdef/BrandLogoSteps.java
@@ -14,10 +14,17 @@ public class BrandLogoSteps extends Base {
 
     @Then("User verifies that {string} is present")
     public void userVerifiesThatIsPresent(String value) {
-        if (value.equals(brandLogo.suppy.getText()))
-        textIsDisplayedAndEnabled("Supply from you print from us", brandLogo.suppy);
+        if (value.equals(brandLogo.suppy.getText())){
+            textIsDisplayedAndEnabled("Supply from you print from us", brandLogo.suppy);
 
-    }
+        }else if(value.equals(brandLogo.bring.getText())){
+            textIsDisplayedAndEnabled("Bring Your Own Tee", brandLogo.bring);
+
+        }else if (value.equals(brandLogo.brands.getText())){
+            textIsDisplayedAndEnabled("Brands", brandLogo.brands);
+        }
+}
+
 
     @Then("User verifies that Print Now button is present")
     public void userVerifiesThatPrintNowButtonIsPresent() {
