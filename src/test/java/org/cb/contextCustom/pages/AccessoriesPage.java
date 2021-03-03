ea8ba36fdec831fbf24ec998f0f6eac4d78ff9cf
Esizsu <esra_izgi@hotmail.com>
2021-03-02 23:37:24 -0800

    Modified AccessoriesPage.java

--- src/test/java/org/cb/contextCustom/pages/AccessoriesPage.java
+++ src/test/java/org/cb/contextCustom/pages/AccessoriesPage.java
@@ -11,14 +11,18 @@ public class AccessoriesPage {
         PageFactory.initElements(MyDriver.get(),this);
     }
 
-    @FindBy(xpath = "(//a[@data-toggle='dropdown'])[1]")
-    public WebElement categories;
+
+    @FindBy(xpath = "//a[@class='nav-link dropdown-toggle pl-0']")
+    public WebElement allProducts;
     //@FindBy(css ="(//a[text()='Accessories'])[1]")
     //xpath="//a[@class='dropdown-item dropdown-toggle'][normalize-space()='Accessories']"
     //linkText="Accessories"
-   //@FindBy(xpath = "(//a[@class='dropdown-item dropdown-toggle'])[1]")
+    //@FindBy(xpath = "(//a[@class='dropdown-item dropdown-toggle'])[1]")
 
-    @FindBy(linkText = "Accessories")
+
+    @FindBy(xpath = "//a[@class='dropdown-item dropdown-toggle'][text()='Accessories']")
+    //@FindBy(xpath = "(//a[text()='Accessories'])[1]")
+//    @FindBy(linkText = "Accessories")
     public WebElement accessories;
 
     @FindBy(xpath = "//li[@class='nav-item dropdown']//li[1]//ul[1]//li[1]")
@@ -27,6 +31,18 @@ public class AccessoriesPage {
     @FindBy(xpath = "//li[@class='breadcrumb-item text-nowrap active']")
     public WebElement textOfAccessories;
 
+    @FindBy(xpath = "(//a[@class='card-img-top d-block overflow-hidden'])[1]")
+    // @FindBy(xpath = "//img[@src='/media/images/EcoToteBack.png']")
+    public WebElement toteBagImage;
 
+    @FindBy(xpath = "(//a[text()='Context'])[1]")
+    public WebElement toteBagBrand;
+
+    @FindBy(xpath = "/html/body/div[3]/div/section/div[2]/div[1]/div/div[1]/h3/a")
+//    @FindBy(linkText = "//a[text()='Organic Tote Bag']")
+    public WebElement organicToteBagText;
+
+    @FindBy(xpath = "//span[@class='text-accent'][1]")
+    public WebElement toteBagPrice;
 
 }
